cfb5e9528700eb7e65de337724c52bd688a1cc25
Andrew Gavegan <zzandrewzz@optusnet.com.au>
2022-04-20 17:22:25 +1000

    added comments

--- project.js
+++ project.js
@@ -2,15 +2,16 @@
 const http = require('http');
 const express = require('express');
 const WebSocket = require('ws'); 
-
-const port = 3000;
+// making a port for server.listen //
+const port = 4567;
 const server = http.createServer(express);
+// creating a new websocket, this is what the ws package is for //
 const wss = new WebSocket.Server( { server })
-
+// function for the connecting and data push and pull? of client data //
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming (data) {
         wss.clients.forEach(function each(client) {
-            if (client != ws && client.reaedyState == WebSocket.OPEN) {
+            if (client != ws && client.readyState == WebSocket.OPEN) {
                 client.send(data);
             }
         })
